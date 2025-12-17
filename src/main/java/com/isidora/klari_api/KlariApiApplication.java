c5dc5a0a4a8d2c0ac7ea063dbc3521f8075a5afd
isialval <isialbayay@gmail.com>
2025-12-17 16:02:05 -0300

    Initial commit: Spring boot project creation

---
+++ src/main/java/com/isidora/klari_api/KlariApiApplication.java
@@ -0,0 +1,13 @@
+package com.isidora.klari_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlariApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlariApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isidora/klari_api/KlariApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isidora.klari_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlariApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
